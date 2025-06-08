3208b391dcf3101cbec479688cde4e4e54668a63
Souvik-025 <souvik.sarkar.cs@gmail.com>
2025-06-08 14:14:17 +0530

    Queue.java

--- Queue.java
+++ Queue.java
@@ -1,75 +1,75 @@
-import java.util.Arrays;
-
-public class Queue {
-    int[] arr;
-    int capacity;
-    int front;
-    int rear;
+public class Queue<T> {
+    private Object[] arr;
+    private int front = -1;
+    private int rear = -1;
+    private int size = 0;
 
     public Queue(int capacity) {
-        this.capacity = capacity;
-        this.arr = new int[capacity];
-        this.front = -1;
-        this.rear = -1;
+        this.arr = new Object[capacity];
     }
 
-    public boolean isEmpty() {
-        return (rear == -1 || front == -1);
+    public final boolean isEmpty()
+    {
+        return front == -1 && rear == -1;
     }
 
-    public boolean isFull() {
-        return (rear + 1) % capacity == front;
+    public final boolean isFull() {
+        return (rear + 1) % arr.length == front;
     }
 
-    public int peek() {
-        return arr[front];
+    public final void enqueue(Object data) {
+        if (isEmpty()) {
+            arr[++front] = arr[++rear] = data;
+        } else {
+            if (!isFull()) {
+                arr[this.rear = (rear + 1) % arr.length] = data;
+            } else
+                throw new QueueOverFlowException("Queue OverFlow");
+        }
+        size++;
     }
 
-    public void enqueue(int data) {
-
-        if (isFull())
-            throw new QueueOverflowException("Queue Overflow");
-
+    public final Object dequeue() {
+        Object data;
         if (isEmpty())
-            arr[rear = front = 0] = data;
-        else
-            arr[rear = (rear + 1) % capacity] = data;
-        System.out.println("Data Enqueued");
-    }
-
-    public int dequeue() {
-        if (isEmpty())
-            throw new QueueUnderflowException("Queue Underflow");
-
-        int data = arr[front];
-
-        if (rear == front)
-            rear = front = -1;
-        else
-            front = (front + 1) % capacity;
-
+            throw new QueueUnderFlowException("Queue Underflow");
+        else {
+            if(front == rear)
+            {
+                data = arr[front];
+                front = rear = -1;
+            }
+            else
+            {
+                data = arr[front];
+                front = (front + 1) % arr.length;
+            }
+        }
+        size--;
         return data;
     }
 
-    @Override
-    public String toString() {
-        String s = "[ ";
-        if (!isEmpty()) {
-            int i = front;
-            while(true)
-            {
-                if (i != rear)
-                s += arr[i] + ", ";
-                else {
-                    s += arr[i];
-                    break;
-                }
-                i = (i + 1) % capacity;
-            }
-        }
-        s += " ]";
-
-        return s;
+    public final int getSize()
+    {
+       return size; 
     }
 
+    public final Object peek()
+    {
+        return !isEmpty() ? arr[front] : null;
+    }
+
+    @Override
+    public String toString()
+    {
+        String s = "[ ";
+        for (int i = front; true ; i = (i + 1) % arr.length)
+        {
+            s += i != rear ? arr[i]+", " : arr[i];
+            if(i == rear)
+                break;
+        }
+        s += " ]";
+        return s;
+    }
 }
