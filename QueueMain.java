942860f6fb006d0f96227b197f3373daf02acda5
Souvik-025 <souvik.sarkar.cs@gmail.com>
2025-06-08 14:15:28 +0530

    QueueMain.java

--- QueueMain.java
+++ QueueMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class QueueMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Queue Capcity");
